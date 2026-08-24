9d97fdecc9d7606a97ee8238c1a86dd68eb4d14b
Vincent Jiang <vincejiang1996@gmail.com>
2026-08-24 11:48:14 -0700

    Add ddb service

--- apps/backend/ingestion/lambdas/lambda_function.py
+++ apps/backend/ingestion/lambdas/lambda_function.py
@@ -1,12 +1,16 @@
 import json
 
+from apps.backend.ingestion.lambdas.services.ddb_service import DDBService
 from apps.backend.ingestion.lambdas.services.price_service import PriceService
 from apps.backend.ingestion.lambdas.services.riftbound_service import RiftboundService
 
 rbService = RiftboundService()
 priceService = PriceService(rbService)
+ddbService = DDBService()
 
 
 def lambda_handler(event, context):
     cards = priceService.get_cards()
+    ddbService.write_cards_price(cards)
+
     return {"statusCode": 200, "body": json.dumps("Successful execution!")}
--- apps/backend/ingestion/lambdas/services/ddb_service.py
+++ apps/backend/ingestion/lambdas/services/ddb_service.py
@@ -6,11 +6,11 @@ from apps.backend.ingestion.lambdas.justtcg_card import JustTCGCard
 
 dynamodb = boto3.resource("dynamodb")
 
-CARD_PRICE_TABLE = "card-price"
-CARD_METADATA_TABLE = "card-metadata"
+CARD_METADATA_TABLE = "CardMetadata"
+CARD_PRICE_TABLE = "CardPrice"
 
 
-class ddbService:
+class DDBService:
     def __init__(self):
         self.ddb = dynamodb
 
@@ -37,6 +37,8 @@ class ddbService:
 
     def write_cards_price(self, cards: list[JustTCGCard]):
         table = self.ddb.Table(CARD_PRICE_TABLE)
+
+        rows_to_write = []
         for card in cards:
             if card.variants:
                 # We only want the pricing for the first variant which should be the lower rarity
@@ -44,6 +46,12 @@ class ddbService:
 
                 for price_history in variant.priceHistory:
                     item = json.loads(price_history.model_dump_json(include={"p", "t"}))
-                    item["card_name"] = card.name
+                    item["name"] = card.name
+                    rows_to_write.append(item)
 
-                    table.put_item(Item=item)
+        try:
+            with table.batch_writer() as batch:
+                for row in rows_to_write:
+                    batch.put_item(Item=row)
+        except Exception as e:
+            print(f"Error writing to DynamoDB: {e}")
