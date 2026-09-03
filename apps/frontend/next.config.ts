5fdfce4cc6db8bfcff377d28dc8d7e8f1498e03c
Vincent Jiang <vincejiang1996@gmail.com>
2026-09-03 15:36:36 -0700

    Refactor name

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
 
-  basePath: "/rift-staples",
+  basePath: "/RBConsultation",
 
   trailingSlash: true,
 
